ab3c5201804b8fecbce11d70f21d8f184fa34b57
Jaylen Dixon <jaylendixon08@gmail.com>
2025-06-23 14:02:31 -0700

    nav bar and account connection

--- src/App.js
+++ src/App.js
@@ -7,23 +7,37 @@ import Search from './components/Search';
 import Home from './components/Home';
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstate from './abis/RealEstate.json';
+import Escrow from './abis/Escrow.json';
 
 // Config
 import config from './config.json';
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.etherum.request({
+        method: 'eth_requestAccounts',
+      });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
   return (
     <div>
-
-      <div className='cards__section'>
-
+      <Navigation account={account} setAccount={setAccount} />
+      <div className="cards__section">
         <h3>Welcome to Millow</h3>
-
       </div>
-
     </div>
   );
 }
